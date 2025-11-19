4baeb5f69374941b8ed389ecc1b00b5e0712d96a
Roy Roberts <rorobert@ucsc.edu>
2025-11-18 22:24:26 -0800

    Query Sample ID v1

--- sql_scripts/create_extraction_metadata_table.sql
+++ sql_scripts/create_extraction_metadata_table.sql
@@ -1,10 +1,10 @@
 CREATE TABLE extraction_metadata (
     extraction_metadata_id INT AUTO_INCREMENT PRIMARY KEY,
     extraction_id VARCHAR(255) NOT NULL UNIQUE,
-    sample_id VARCHAR(255) NOT NULL UNIQUE,
+    sample_id VARCHAR(255) NOT NULL,
     extraction_date DATE NOT NULL,
     extraction_method VARCHAR(255) NOT NULL,
-    amount_of_sample_used DECIMAL(10, 2) NOT NULL,
+    amount_of_sample_used DECIMAL(10, 2),
     extractor_name VARCHAR(255) NOT NULL,
     contact INT NOT NULL, 
     notes TEXT,
